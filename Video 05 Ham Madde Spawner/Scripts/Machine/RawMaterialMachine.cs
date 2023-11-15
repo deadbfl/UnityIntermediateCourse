62088fb565c068bb0b168248b30f43274b7a494c
Ahmet Emre YILDIZ <ahmetemreyildiz51@gmail.com>
2023-11-15 11:28:57 +0300

    Video 6 Eklendi

---
+++ Video 05 Ham Madde Spawner/Scripts/Machine/RawMaterialMachine.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RawMaterialMachine : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;
+
+    [SerializeField] private RawMaterialScriptableObject rawMaterialValue;
+
+    private RawMaterialVariables rawMaterial;
+
+    private void Awake()
+    {
+        rawMaterial.SetValue(rawMaterialValue.variables);
+
+        StartCoroutine(Spawner());
+    }
+
+    private IEnumerator Spawner()
+    {
+        while (true)
+        {
+            Instantiate(rawMaterial.GetRawMaterialPrefab(), spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(rawMaterial.SpawnTimer);
+        }
+    }
+}
+
+[Serializable]
+public struct RawMaterialVariables
+{
+    [SerializeField] private RawMaterialType currentType;
+    [SerializeField] private GameObject rawMaterialPrefab;// Spawnlatacagim Nesne
+    [SerializeField] private float cost;
+    [SerializeField] private float spawnTimer;
+    public float SpawnTimer { get { return spawnTimer; } set { spawnTimer = value; } }
+
+    public void SetValue(RawMaterialVariables values)
+    {
+        SetRawMaterialType(values.currentType);
+        SetRawMaterialPrefab(values.rawMaterialPrefab);
+        SetCost(values.cost);
+        SpawnTimer = values.spawnTimer;
+    }
+
+    #region CostEncapsulation
+    public float GetCost()
+    {
+        return cost;
+    }
+
+    public void SetCost(float value)
+    {
+        cost = MathF.Abs(value);
+    }
+    #endregion
+
+    #region CurrentTypeEncapsuliton
+    public RawMaterialType GetRawMaterialType()
+    {
+        return currentType;
+    }
+
+    public void SetRawMaterialType(RawMaterialType value)
+    {
+        currentType = value;
+    }
+    #endregion
+
+    #region PrefabEncapulation
+    public GameObject GetRawMaterialPrefab()
+    {
+        return rawMaterialPrefab;
+    }
+    public void SetRawMaterialPrefab(GameObject value)
+    {
+        rawMaterialPrefab = value;
+    }
+    #endregion
+}
